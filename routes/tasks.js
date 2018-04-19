7819f18c357a42ed70f22ec9d2a810fa191f3c85
soufiane <soufiane.elmoudden.15@gmail.com>
2018-04-19 13:19:12 +0100

    Front end angularjs version 1

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,18 +1,5 @@
 var express = require('express');
 var router = express.Router();
-router.post('/task', function(req, res, next){
-    var task = req.body;
-    if(!task.title || !(task.isDone + '')){
-        res.status(400);
-        res.json({
-            "error": "Bad Data"
-        });
-    } else {
-        db.tasks.save(task, function(err, task){
-            if(err){
-                res.send(err);
-            }
-            res.json(task);
-        });
-    }
+router.get('/tasks', function(req, res, next){
+   
 });
