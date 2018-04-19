4eff3d33577d507ad824b2e94ab0a80c5f554d55
soufiane <soufiane.elmoudden.15@gmail.com>
2018-04-19 12:47:22 +0100

    task api route

---
+++ routes/tasks.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+router.post('/task', function(req, res, next){
+    var task = req.body;
+    if(!task.title || !(task.isDone + '')){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.tasks.save(task, function(err, task){
+            if(err){
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }
+});
